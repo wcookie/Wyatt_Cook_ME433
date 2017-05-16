a7083b976eb4fde5e05adc8328fcc44ff5e09277
Wyatt Cook <wcookeverton@gmail.com>
2017-05-16 14:15:30 -0500

    hw11 finished

--- HelloME433/app/src/main/java/com/example/wcook/hellome433/MainActivity.java
+++ HelloME433/app/src/main/java/com/example/wcook/hellome433/MainActivity.java
@@ -2,12 +2,43 @@ package com.example.wcook.hellome433;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
-
+    SeekBar myControl;
+    TextView myTextView;
+    //int progressChanged = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myControl = (SeekBar) findViewById(R.id.seek1);
+
+        myTextView = (TextView) findViewById(R.id.textView01);
+        myTextView.setText("Hello Senior Lecturer Nicholas");
+        setMyControlListener();
+    }
+    private void setMyControlListener() {
+        myControl.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+
+            int progressChanged = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChanged = progress;
+
+                myTextView.setText("The value is: "+ progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
